2be70850b7846e7d5741b66a4b367dbc68e723d2
fengye92 <fengye920110@gmail.com>
2014-07-30 17:05:51 +0800

    add jar

--- src/com/iel/hos/actions/DoctorsAction.java
+++ src/com/iel/hos/actions/DoctorsAction.java
@@ -1,8 +1,12 @@
 package com.iel.hos.actions;
 
+import com.iel.hos.services.DoctorsService;
+
 public class DoctorsAction {
 	private String user;
 	private String passwd;
+	private DoctorsService docService;
+	
 	public void setUser(String user) {
 		this.user = user;
 	}
@@ -13,14 +17,19 @@ public class DoctorsAction {
 	/**
 	   * µÇÂ¼
 	   * @return
+	 * @throws Exception 
 	   */
-		public String checkLogin(){
+		public String checkLogin() throws Exception{
 			System.out.println(user);
 			System.out.println(passwd);
+			
+			this.docService=new DoctorsService();
+			this.docService.checkLogin(user,passwd);
 			if(this.user.equals("wyk")&&this.passwd.equals("123"))
 			return "success";
 				else
 			return "error";
+			
 		
 		}
 }
---
+++ src/com/iel/hos/services/DoctorsService.java
@@ -0,0 +1,16 @@
+package com.iel.hos.services;
+
+import java.util.Map;
+
+import com.iel.hos.dao.DoctorsDao;
+
+public class DoctorsService {
+	private DoctorsDao docDao;
+	
+	public Map<String, String> checkLogin(String user, String passwd) throws Exception{
+		docDao=new DoctorsDao();
+		
+		docDao.checkLogin();
+		return null;
+	}
+}
