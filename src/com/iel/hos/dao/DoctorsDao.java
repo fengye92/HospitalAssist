4aa2733413cd6fe4bdfdc7890a259ef710626069
fengye92 <fengye920110@gmail.com>
2014-07-31 09:57:37 +0800

    tab change~

--- src/com/iel/hos/dao/DoctorsDao.java
+++ src/com/iel/hos/dao/DoctorsDao.java
@@ -1,32 +1,20 @@
 package com.iel.hos.dao;
 
-import java.io.IOException;     
-import java.util.ArrayList;     
-import java.util.List;           
+import java.io.IOException;              
 import org.apache.hadoop.conf.Configuration;     
 import org.apache.hadoop.hbase.HBaseConfiguration;     
 import org.apache.hadoop.hbase.HColumnDescriptor;     
-import org.apache.hadoop.hbase.HTableDescriptor;     
-import org.apache.hadoop.hbase.KeyValue;     
-import org.apache.hadoop.hbase.MasterNotRunningException;     
-import org.apache.hadoop.hbase.ZooKeeperConnectionException;     
-import org.apache.hadoop.hbase.client.Delete;     
-import org.apache.hadoop.hbase.client.Get;     
+import org.apache.hadoop.hbase.HTableDescriptor;         
 import org.apache.hadoop.hbase.client.HBaseAdmin;     
-import org.apache.hadoop.hbase.client.HTable;     
-import org.apache.hadoop.hbase.client.Result;     
-import org.apache.hadoop.hbase.client.ResultScanner;     
-import org.apache.hadoop.hbase.client.Scan;     
-import org.apache.hadoop.hbase.client.Put;     
-import org.apache.hadoop.hbase.util.Bytes;     
+    
           
 public class DoctorsDao {       
 	private static Configuration configuration;
-    	
-   
+	
+	
 	public void checkLogin() throws IOException{    
-       	configuration = null;    
-		configuration =HBaseConfiguration.create();  
+		configuration = null;    
+       	configuration =HBaseConfiguration.create();  
 		configuration.set("hbase.zookeeper.quorum", "192.168.0.201");
 		configuration.set("hbase.zookeeper.property.clientPort", "2181");
 		configuration.set("hbase.master", "192.168.0.201");
@@ -41,37 +29,7 @@ public class DoctorsDao {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		/*HBaseAdmin admin = new HBaseAdmin(configuration);
-			
-			System.out.println(admin.getTableNames());
-			
-			HTableDescriptor ht = new HTableDescriptor("table1");
-			HColumnDescriptor hc1 = new HColumnDescriptor("c1");
-			HColumnDescriptor hc2 = new HColumnDescriptor("c2");
-			hc1.setMaxVersions(1);       
-			hc2.setMaxVersions(3);
-			ht.addFamily(hc1); 
-			ht.addFamily(hc2);        
-			admin.createTable(ht);   
-			admin.close();
-			
-			HTable table = new HTable(configuration, "table1");        
-			String aString = "b";         
-			Put put = new Put(Bytes.toBytes(aString));                   
-			put.add(Bytes.toBytes("c1"), Bytes.toBytes("cc1"), Bytes.toBytes(100));
-			put.add(Bytes.toBytes("c2"), Bytes.toBytes("cc2"), Bytes.toBytes(101));
-			table.put(put);          Scan scan = new Scan();         
-			ResultScanner ss = table.getScanner(scan);        
-			for (Result result : ss) {         
-				byte[] did = result.getValue(Bytes.toBytes("c1"), Bytes.toBytes("cc1"));          
-				int a = Bytes.toInt(did);          System.out.println(a);         }         
-			ResultScanner ss1= table.getScanner(Bytes.toBytes("c1"));        
-			for (Result result : ss1) {        
-				byte[] did = result.getRow();         
-				String a = Bytes.toString(did);         
-				System.out.println(a);         }         
-			table.close(); */               
-        }
+	}
         
 	public static void creatTable(String tableName, String[] familys) throws Exception {       
          HBaseAdmin admin = new HBaseAdmin(configuration);       
--- src/com/iel/hos/services/DoctorsService.java
+++ src/com/iel/hos/services/DoctorsService.java
@@ -1,7 +1,6 @@
 package com.iel.hos.services;
 
 import java.util.Map;
-
 import com.iel.hos.beans.Doctor;
 import com.iel.hos.dao.BaseDao;
 import com.iel.hos.dao.DoctorsDao;
@@ -19,9 +18,11 @@ public class DoctorsService {
 		column[2]="test2";
 		base.creatTable("columnTest", column);
 		base.addColumnFamily("columnTest", "test3");
+		
 		//base.addRecord("wyktest", "111", "test1", "", "2014/7/30");
 		//base.addRecord("wyktest", "111", "test2", "", "2014/7/31");
 		base.getAllRecord("wyktest");
+		
 		//base.creatTable("aasf", column);
 		//docDao.checkLogin();
 		return null;
